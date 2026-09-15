925b2a5c6eb25edb43a4c6577efaf7cbc1d54e49
Lukazx <nattananpunpetch@gmail.com>
2026-09-15 21:48:17 +0700

    Add balloon animation feature with HTML, CSS, and JavaScript files

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,36 @@
+const balloon = document.getElementById('balloon');
+
+const colors = ['red', 'green', 'blue'];
+let colorIndex = 0;
+let size = 200;
+
+function updateBalloon() {
+    balloon.style.width = size + 'px';
+    balloon.style.height = size + 'px';
+    balloon.style.backgroundColor = colors[colorIndex];
+}
+
+balloon.addEventListener('click', () => {
+    size += 10;
+
+    if (size > 420) {
+        size = 200;
+        colorIndex = 0;
+    } else {
+        colorIndex = (colorIndex + 1) % colors.length;
+    }
+
+    updateBalloon();
+});
+
+balloon.addEventListener('mouseleave', () => {
+    if (size > 200) {
+        size -= 5;
+        if (size < 200) {
+            size = 200;
+        }
+
+        colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+        updateBalloon();
+    }
+});
